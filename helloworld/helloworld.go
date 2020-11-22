7cb7068025e161d230574a7f17b2553140ff6b09
Junious <chengjy42@gmail.com>
2020-11-23 00:56:53 +0800

    init

---
+++ helloworld/helloworld.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+	"runtime"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+const (
+	// 将 1 左移 100 位来创建一个非常大的数字
+	// 即这个数的二进制是 1 后面跟着 100 个 0
+	Big = 1 << 100
+	// 再往右移 99 位，即 Small = 1 << 1，或者说 Small = 2
+	Small = Big >> 99
+)
+
+
+
+func main() {
+	fmt.Println("Hello, 世界")
+	//fmt.Println("My favorite number is", math.rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(add(42, 13))
+	//a, b := swap("hello", "world")
+	//fmt.Println(a, b)
+	fmt.Println(split(17))
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+
+	i, j := 42, 2701
+	p := &i         // 指向 i
+	fmt.Println(*p) // 通过指针读取 i 的值
+	*p = 21         // 通过指针设置 i 的值
+	fmt.Println(i)  // 查看 i 的值
+
+	p = &j         // 指向 j
+	*p = *p / 37   // 通过指针对 j 进行除法运算
+	fmt.Println(j) // 查看 j 的值
+
+
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	var s []int
+	s = append(s, 0, 1, 2, 3, 4, 5, 7)
+	printSlice(s)
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i:=0; i<10; i++ {
+		fmt.Println(z)
+		z -= (z*z - x) / (2*z)
+	}
+	return z
+}
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func Pic(dx, dy int) [][]uint8 {
+	data := make([][]uint8, dy)
+	for i := range data {
+		data[i] = make([]uint8, dx)
+		for j := range data[i] {
+			data[i][j] = uint8((i+j)/2)
+		}
+	}
+	return data;
+}
+  
