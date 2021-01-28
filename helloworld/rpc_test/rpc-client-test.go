61d5a47a288993b458f3a0ad3ba26291efb3a9f9
Junious <chengjy42@gmail.com>
2021-01-28 18:41:40 +0800

    rpc hello world

---
+++ helloworld/rpc_test/rpc-client-test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+
+	"./context"
+)
+
+type HelloServiceClient struct {
+	*rpc.Client
+}
+
+var _ context.HelloServiceInterface = (*HelloServiceClient)(nil)
+
+func DialHelloService(network, address string) (*HelloServiceClient, error) {
+	c, err := rpc.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	return &HelloServiceClient{Client: c}, nil
+}
+
+func (p *HelloServiceClient) Hello(request string, reply *string) error {
+	return p.Client.Call(context.HelloServiceName+".Hello", request, reply)
+}
+
+func main() {
+	client, err := DialHelloService("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	var reply string
+	err = client.Hello("world", &reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(reply)
+}
